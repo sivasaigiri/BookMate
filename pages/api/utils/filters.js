88d02b1b94c4e633ff74360df8c0da943e618bc8
SIVA SAI GIRI SEELAM <129787325+sivasaigiri@users.noreply.github.com>
2025-07-05 23:49:27 +0530

    Update filters.js

--- pages/api/utils/filters.js
+++ pages/api/utils/filters.js
@@ -2,7 +2,7 @@ import database from '../../../utils/database';
 
 const fetchfilters = async (req, res) => {
 	const filters = await datation('utils').findOne({name: "filters"}));
-	res.status(200).json(filters.payload);
+	
 }
 
 export default fetchfilters;
